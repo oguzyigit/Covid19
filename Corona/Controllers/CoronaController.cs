3a0f544834f50ab320e73697f10137a13c34a971
oguzyigit <edeoguz@hotmail.com>
2020-03-28 19:22:54 +0300

    Initial check-in of module Corona

---
+++ Corona/Controllers/CoronaController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Corona.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Corona.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoronaController : ControllerBase
+    {
+        public CoronaController()
+        {
+        }
+
+        [HttpGet]
+        public CountryData Get()
+        {
+            CountryData countryDatas = DownloadAndDeserializeJsonData("https://pomber.github.io/covid19/timeseries.json");
+            return countryDatas;
+        }
+
+        private CountryData DownloadAndDeserializeJsonData(string url)
+        {
+            using (var webClient = new WebClient())
+            {
+                var jsonData = string.Empty;
+                try
+                {
+                    jsonData = webClient.DownloadString(url);
+                }
+                catch (Exception) { }
+
+                return 
+                    JsonConvert.DeserializeObject<CountryData>(jsonData);
+            }
+        }
+    }
+}
---
+++ Corona/Entity/CountryData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Corona.Entity
+{
+    public class CountryData
+    {
+        public CountryData()
+        {
+        }
+
+        [JsonProperty("Turkey")]
+        public Data[] Turkey { get; set; }
+        [JsonProperty("Italy")]
+        public Data[] Italy { get; set; }
+
+    }
+
+    public class Data
+    {
+        public Data()
+        {
+
+        }
+
+        public DateTime Date { get; set; }
+        public int Confirmed { get; set; }
+        public int Deaths { get; set; }
+        public int Recovered { get; set; }
+    }
+}
